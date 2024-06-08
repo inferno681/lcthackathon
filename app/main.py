210ce7c3849cdf763d2a27a9fe5f4645949fd490
Vasilii Stakrotckii <inferno681@gmail.com>
2024-06-08 16:12:54 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -22,7 +22,6 @@ async def add_process_time_header(request: Request, call_next):
     response = await call_next(request)
     process_time = time.time() - start_time
     response.headers["X-Process-Time"] = str(process_time)
-    print(process_time)
     return response
 app.include_router(api_router)
 

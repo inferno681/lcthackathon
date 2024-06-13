4283aedcc8daaa9d91fb17a29f87a49cf9872f96
Vladimir <59032638+Vladimir-pro@users.noreply.github.com>
2024-06-14 00:23:44 +0300

    Update ml.py
    Выделил распознавание аудио в отдельный сервис, добавил функцию перевода с английского на русский

--- worker/ml.py
+++ worker/ml.py
@@ -1,6 +1,5 @@
 import os
-import torch
-from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from openai import OpenAI
 from ffmpeg.asyncio import FFmpeg
 from uuid import uuid4
 from ollama import AsyncClient
@@ -9,66 +8,74 @@ from ollama._types import Options
 
 from services import config, convert_text_to_embeddings
 PATH_TO_FILES = './temp/'
-
+GPU_HOST = 'http://89.179.242.31'
 
 ollama = AsyncClient(host=config.OLLAMA_SERVER)
+options = Options(temperature=0.1, max_tokens=120)
+# Объект подключения к серверу транскоибции аудиодорожки
+client = OpenAI(api_key="cant-be-empty", base_url=f'{GPU_HOST}:8081/v1/')
 
+# Задаем подключение к серверу эмбеддингов
+embeddings = HuggingFaceHubEmbeddings(model=f'{GPU_HOST}:8082')
+
+# Создаем объект подключения к серверу ollama, на котором поднята Llava
+ollama = Client(host=f'{GPU_HOST}:8083')
+
+# Устанавливаем температуру 0.1 для запросов к модели Llava
 options = Options(temperature=0.1, max_tokens=120)
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+# Точка доступа к серверу переводов
+TRANSLATE_HOST = f'{GPU_HOST}:8084/api/v1/translate'
 
-
-model_id = "openai/whisper-base"
-model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id,
-    torch_dtype=torch_dtype,
-    low_cpu_mem_usage=True,
-    use_safetensors=True,
-)
-model.to(device)
-
-processor_id = "openai/whisper-base"
-processor = AutoProcessor.from_pretrained(processor_id)
-
-
-pipe = pipeline(
-    "automatic-speech-recognition",
-    model=model,
-    tokenizer=processor.tokenizer,
-    feature_extractor=processor.feature_extractor,
-    max_new_tokens=128,
-    chunk_length_s=30,
-    return_language=True,
-    batch_size=16,
-    return_timestamps=True,
-    torch_dtype=torch_dtype,
-    device=device,
-)
 
 
 async def add_video(link):
-
+    # парсинг видеофайла, выделение аудодорожки и первого кадра
     audio_file, face = await parse_video(link)
-
+    
+    # Транскрибция аудио
     text_from_audio = audio_transcribe(audio_file)
     os.remove(audio_file)
+    
+    # Формируем описание видео по первому кадру
+    text_from_video = await image_recognition(face)
 
-    text_from_video = await image_recignition(face)
-    print(text_from_video)
-    t_text = f'{text_from_audio} {text_from_video}'.strip()
-    if not t_text:
-        t_text = "No sound"
-    embedding = await convert_text_to_embeddings(t_text)
-    res = {
-        'voise_description': text_from_audio,
-        'image_description': text_from_video,
-        'full_description': t_text,
-        'face': face,
-        'embedding_description': embedding,
-    }
+    # Переводим на русский язык, т.к. модель распознавания видео возвращает ответ на английском языке
+    text_from_video = translate(text_from_video).strip('"')
+
+     # Составление эмбеддингов, каждое предложение отдельный вектор эмбеддингов
+    text_embed = []
+    # Эмбеддинги аудиозаписи по предложениям
+    if text_from_audio != '':
+        for predl in text_from_audio.split("."):
+            if (not str(predl.strip()).isdigit()) and (predl.strip() != ''):
+                text_embed.append(await convert_text_to_embeddings(predl[:250]))
+    
+    # Эмбеддинги описаний видео по предложениям
+    if text_from_video != '':
+        for predl in text_from_video.split("."):
+            if (not str(predl.strip()).isdigit()) and (predl.strip() != ''):
+                text_embed.append(await convert_text_to_embeddings(predl[:250]))
+
+    res={
+         'voise_description':text_from_audio,
+         'image_description':text_from_video,
+         'face': face,
+         'embedding': text_embed, # Массив эмбеддингов каждого предложения
+         }
+    
+    # t_text = f'{text_from_audio} {text_from_video}'.strip()
+    # if not t_text:
+    #     t_text = "No sound"
+    # embedding = await convert_text_to_embeddings(t_text)
+    # res = {
+    #     'voise_description': text_from_audio,
+    #     'image_description': text_from_video,
+    #     'full_description': t_text,
+    #     'face': face,
+    #     'embedding_description': embedding,
+    # }
     return res
-
 
 async def parse_video(link):
     out_audio = f'{PATH_TO_FILES}{uuid4()}.wav'
@@ -93,20 +100,43 @@ async def parse_video(link):
 
 
 def audio_transcribe(link):
-    result = pipe(link, generate_kwargs={"task": "transcribe"})
-
-    if result['chunks'][0]['language'] != 'russian':
-        res = ''
+    # Открываем файл аудиодорожки
+    audio_file = open(link, 'rb')
+    # Устанавливаем параметры расспознавания
+    transcript = client.audio.transcriptions.create(
+                            model="Systran/faster-whisper-base",
+                            file=audio_file,
+                            temperature=0.0,
+                            )
+    # Расспознаем аудиодорожку
+    translated = transcript.text
+    if len(translated)>4:
+        # Проверка языка расспознанного текста
+        # Как правило язык не на русском означает фоновую музыку
+        if bool(re.search('[а-яА-Я]', translated[5])):
+            res = translated
+        else:
+            res = ''
     else:
-        res = result['text']
+        res = translated
 
     return res
 
-
-async def image_recignition(image_path):
+# Функция описания видео по первому кадру
+async def image_recognition(image_path):
     res = await ollama.generate(
         model='llava-llama3-int4',
-        prompt='What is this a picture of shortly? Аnswer me briefly 3 main items.',
+        prompt='What is this a picture of shortly? Аnswer me briefly 1 main item.',
         images=[image_path],
         options=options)
     return res['response']
+
+# Функция перевода с английского на русский
+async def translate(input_str):
+    response = requests.post(TRANSLATE_HOST,
+                             params={'input_str': input_str}
+                             )
+    if response:
+        return  response.content.decode('utf-8')
+    else:
+        return ''

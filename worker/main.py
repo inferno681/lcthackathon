c97f55edd1f0171de55100c2aab663b51435bc8c
Vasilii Stakrotckii <inferno681@gmail.com>
2024-06-16 22:58:47 +0200

    fix

--- worker/main.py
+++ worker/main.py
@@ -28,8 +28,7 @@ async def add_video_task(ctx: dict, data: dict):
             obj.embeddings = response["embedding"]
             session.add(obj)
             await session.commit()
-        except Exception as e:
-            print(e)
+        except Exception:
             raise Retry(defer=ctx["job_try"] * 5)
         await send_file_to_fastapi(
             response["face"], config.SCREENSHOT_UPLOAD_LINK
